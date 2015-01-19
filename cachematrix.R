e1edf7a6a2700e12cccc332ebdcf23898c9d5d93
Andre Kempe <a.kempe@free.fr>
2015-01-19 14:48:54 +0100

    programming assignment 2 completed, empty functions filled with my code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+##
+## Two functions for compiling, caching and retrieving the inverse of a martix
+## ----------------------------------------------------------------------------
 
-## Write a short comment describing this function
+## Function for creating an object that wraps a matrix and caches its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(m) {
+    x <<- m
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(i) inv <<- i
+  getinv <- function() inv
+  list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
+## Function for calculating or retrieving from cache the inverse of a "wrapped" matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if (!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data)
+  x$setinv(inv)
+  inv
 }
+
